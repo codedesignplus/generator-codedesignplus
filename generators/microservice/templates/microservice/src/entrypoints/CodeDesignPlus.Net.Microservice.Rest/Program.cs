8a0cba850155bf00edbe00c86672e5ee196d4fb4
CodeDesignPlus <codedesignplus@outlook.com>
2025-04-11 17:13:36 -0500

    feat: add Helm charts for ms-archetype services and update .gitignore for user files

--- generators/microservice/templates/microservice/src/entrypoints/CodeDesignPlus.Net.Microservice.Rest/Program.cs
+++ generators/microservice/templates/microservice/src/entrypoints/CodeDesignPlus.Net.Microservice.Rest/Program.cs
@@ -1,13 +1,15 @@
 using CodeDesignPlus.Net.Microservice.Commons.EntryPoints.Rest.Middlewares;
+using CodeDesignPlus.Net.Microservice.Commons.EntryPoints.Rest.Resources;
 using CodeDesignPlus.Net.Microservice.Commons.EntryPoints.Rest.Swagger;
 using CodeDesignPlus.Net.Microservice.Commons.FluentValidation;
+using CodeDesignPlus.Net.Microservice.Commons.HealthChecks;
 using CodeDesignPlus.Net.Microservice.Commons.MediatR;
 using CodeDesignPlus.Net.Redis.Cache.Extensions;
 using CodeDesignPlus.Net.Vault.Extensions;
-using NodaTime;
 using NodaTime.Serialization.SystemTextJson;
 
-var builder = WebApplication.CreateSlimBuilder(args);
+
+    var builder = WebApplication.CreateSlimBuilder(args);
 
 Serilog.Debugging.SelfLog.Enable(Console.Error);
 
@@ -19,9 +21,8 @@ builder.Services
     .AddControllers()
     .AddJsonOptions(opt => opt.JsonSerializerOptions.ConfigureForNodaTime(DateTimeZoneProviders.Tzdb));
 
-
 builder.Services.AddEndpointsApiExplorer();
-
+builder.Services.AddCors();
 builder.Services.AddVault(builder.Configuration);
 builder.Services.AddRedis(builder.Configuration);
 builder.Services.AddMongo<CodeDesignPlus.Net.Microservice.Infrastructure.Startup>(builder.Configuration);
@@ -34,14 +35,21 @@ builder.Services.AddMediatR<CodeDesignPlus.Net.Microservice.Application.Startup>
 builder.Services.AddSecurity(builder.Configuration);
 builder.Services.AddCoreSwagger<Program>(builder.Configuration);
 builder.Services.AddCache(builder.Configuration);
-
+builder.Services.AddResources<Program>(builder.Configuration);
+builder.Services.AddHealthChecksServices();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-app.UseCoreSwagger();
+app.UseCors(builder => builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+);
+app.UseExceptionMiddleware();
+app.UseHealthChecks();
+app.UseCodeErrors();
 
-app.UseMiddleware<ExceptionMiddleware>();
+app.UseCoreSwagger();
 
 app.UseHttpsRedirection();
 
---
+++ generators/microservice/templates/microservice/tests/integration/CodeDesignPlus.Net.Microservice.gRpc.Test/Helpers/ServerCollectionDefinition.cs
@@ -0,0 +1,10 @@
+namespace CodeDesignPlus.Net.Microservice.gRpc.Test.Helpers;
+
+/// <summary>
+/// Define the collection of the server.
+/// </summary>
+[CollectionDefinition(ServerCollectionFixture<Program>.Collection)]
+public class ServerCollectionDefinition: ICollectionFixture<ServerCollectionFixture<Program>>
+{
+
+}
